cecc83555646a2067d8b85503141fb0d977594a3
s <as>
2023-12-17 15:10:00 -0500

    ready for deploy

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import LandingProvider from './componets/providers/index.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <LandingProvider>
     <App />
